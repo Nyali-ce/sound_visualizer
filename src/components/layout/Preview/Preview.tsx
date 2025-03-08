9eeec0138b25b69c3510a4c13fb3a90423b2c02a
Nyali_ce <nathan@alconat.ca>
2025-03-08 14:28:38 -0500

    feat: update Preview component to use background color from context

--- src/components/layout/Preview/Preview.tsx
+++ src/components/layout/Preview/Preview.tsx
@@ -3,12 +3,11 @@ import { useEffect, useRef, useContext } from 'react';
 import { OptionContext } from '../../../Contexts/OptionContext';
 
 const Preview = () => {
-
+    const { backgroundColor } = useContext(OptionContext);
 
     return (
         <div id='preview-container'>
-            <div id='preview'>
-                {/* preview */}
+            <div id='preview' style={{backgroundColor: backgroundColor}}>
             </div>
         </div>
     );
